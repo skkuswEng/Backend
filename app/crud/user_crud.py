85131127433e2e9932e972891946e36cae7b5d0e
Taehyun An <thahn1230@gmail.com>
2024-11-09 21:27:45 +0900

    Add Naive Sign-up Module

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -10,12 +10,52 @@ engine = Database().get_engine()
 # 유저가 DB에 없다면 빈 DataFrame을 보낼 것임
 def user_login(id: str, password: str):
     query = """
-    SELECT * FROM sweng.user
-    WHERE id = %s and  password = %s;
+    SELECT * FROM user
+    WHERE id = %s and password = %s;
     """
 
     params = (id, password)
     user_df = pd.read_sql(query, engine, params=params)
     return user_df
 
+# 회원가입
+# 이미 회원가입 한 유저인지 확인
+def check_duplicate_user(id: str):
+    query = """
+    SELECT * FROM user
+    WHERE id = %s
+    """
 
+    params = (id, )
+    user_df = pd.read_sql(query, engine, params=params)
+
+    return user_df
+
+# 일단 is_admin은 무조건 False로 박아버리긔 ~
+def user_signup(user_data: dict):
+    # 이미 회원가입 되어 있는 유저이면 오류 발생
+    # 아이디 Front로 넘겨서 가입되어 있는 아이디 보내주는 것 좋다고 봄(추후 해보자)
+    if check_duplicate_user(user_data["student_id"]).empty:
+        return False
+
+    try:
+        with Session(engine) as session:
+            session.execute(
+                text(
+                    """
+                    INSERT INTO user VALUES (:id, :password, :name,
+                     :is_admin, :email);
+                    """
+                ),
+                {
+                    "id": user_data["student_id"], "password": user_data["password"],
+                    "name": user_data["student_name"], "is_admin": False, "email": user_data["email"]
+                }
+            )
+
+            session.commit()
+    except Exception as e:
+        print("Error Occurred: ", e)
+        session.rollback()
+        return False
+    return True
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import JSONResponse
 
-from app.schemas.user.request import LoginRequest, SimulationParamsRequest
-from app.schemas.user.response import LoginResponse
+from app.schemas.user.request import LoginRequest, SignUpRequest
+from app.schemas.user.response import LoginResponse, SignUpResponse
 
 from backend.app.handlers.handler import LoginError
 
-from app.crud.user_crud import user_login
+from app.crud.user_crud import user_login, user_signup
 
 router = APIRouter()
 
@@ -20,6 +20,22 @@ async def login(request: LoginRequest):
     else:
         # 사용자 정보가 DB에 있음
         return LoginResponse(
-            status= "success",
-            message= "Succesfully Logged in"
+            status="success",
+            message="Successfully Logged in"
+        )
+    
+@router.post("/user/register", response_model=SignUpResponse)
+async def signup(request: SignUpRequest):
+    # 회원가입 성공여부
+    is_valid = user_signup(request)
+    
+    if is_valid :
+        return SignUpResponse(
+            status="success",
+            message="Successfully Signed Up"
+        )
+    else :
+        return SignUpResponse(
+            status="failure",
+            message="Try again"
         )
