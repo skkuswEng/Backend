1236a87000070a20ace34ade2ee0af7a49f6e43c
Jin <smsh0722@gmail.com>
2024-11-24 22:42:10 +0900

    httpExceptionHandler to custom UserError

--- app/routers/seat_router.py
+++ app/routers/seat_router.py
@@ -37,19 +37,19 @@ async def ReserveSeat(request: ReserveSeatRequest ):
     seat_reservation_df = getUserSeatReservation( request.student_id )
     # reponse
     if seat_df.empty: 
-        raise HTTPException( status_code=404, detail="요청한 seat_number가 존재하지 않습니다") # 확인
+        raise UserError( status_code=404, detail="요청한 seat_number가 존재하지 않습니다") # 확인
     else :
         if seat_df["is_reserved"].any() == True :
-            raise HTTPException( status_code=409, detail="이미 배정된 좌석입니다") #확인
+            raise UserError( status_code=409, detail="이미 배정된 좌석입니다") #확인
         if room_reservation_df.empty == False :
-            raise HTTPException( status_code=409, detail="이미 해당 시간에 스터디룸 예약한 상황입니다") # check 필요
+            raise UserError( status_code=409, detail="이미 해당 시간에 스터디룸 예약한 상황입니다") # check 필요
         if seat_reservation_df.empty == False :
-            raise HTTPException(status_code=409, detail="이미 다른 좌석을 예약한 상황입니다.") #확인
+            raise UserError(status_code=409, detail="이미 다른 좌석을 예약한 상황입니다.") #확인
 
         result = reserveSeat( request.student_id, request.seat_number, request.reservation_date )
         
         if result == False:
-            raise HTTPException( status_code=500, detail="좌석 배정 중 db 오류")
+            raise UserError( status_code=500, detail="좌석 배정 중 db 오류")
         return SeatReserveResponse(
             message= "Reservation Complete"
         )
@@ -77,17 +77,17 @@ async def SearchReservedSeat( student_id: str= Query(...) ):
 async def UnreserveSeat( request: UnreserveSeatRequest ):
     seat_reservation_df = getUserSeatReservation( request.student_id )
     if seat_reservation_df.empty == True :
-        raise HTTPException( status_code=409, detail="해당 유저가 예약한 좌석이 없습니다.")
+        raise UserError( status_code=409, detail="해당 유저가 예약한 좌석이 없습니다.")
     
     #print( "df: ", int( seat_reservation_df.seat_number ) )
     #print( "req: ", request.seat_number )
     if int(seat_reservation_df.seat_number) == request.seat_number :
         result = unreserveSeat( request.seat_number )
         if result == False :
-            raise HTTPException(status_code=409, detail="좌석 반납중 서버 DB에서 에러 발생")
+            raise UserError(status_code=409, detail="좌석 반납중 서버 DB에서 에러 발생")
         else :
             return SeatUnreserveResponse(
                 message="Unreserve completed"
             )
     else:
-        raise HTTPException( status_code=400, detail="요청 오류 있음")
+        raise UserError( status_code=400, detail="요청 오류 있음")
